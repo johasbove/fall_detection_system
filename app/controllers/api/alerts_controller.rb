0f6f4bd6a794d6dc9775a9f5e39f2012c8bc1b62
Johanna Salazar Bove <13022628+johasbove@users.noreply.github.com>
2023-10-18 19:52:44 +0200

    Level 2
    Better alert json response when success

--- app/controllers/api/alerts_controller.rb
+++ app/controllers/api/alerts_controller.rb
@@ -3,7 +3,13 @@ class Api::AlertsController < ApplicationController
     alert = Alerts::IncomingAlertBuilder.new(alert_params.to_h).build_alert
 
     if alert[:error].nil? && alert.save
-      render json: { success: true, alert: alert }
+      render json: {
+        success: true,
+        alert: alert.as_json(
+          except: [:id, :created_at, :updated_at, :device_id],
+          include: { device: { only: :sim_sid } }
+        )
+      }
     elsif alert[:error]
       render json: { success: false, error: alert[:error] }, status: 400
     else
